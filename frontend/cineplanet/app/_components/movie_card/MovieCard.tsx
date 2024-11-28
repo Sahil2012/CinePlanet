fcda96a17b0afe88f5bceab8cef9383c22004463
Tanmay Dhanu <tdhanu74@gmail.com>
2024-11-28 11:06:15 +0530

    Adding MovieCard

---
+++ frontend/cineplanet/app/_components/movie_card/MovieCard.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Box, Card, Inset } from "@radix-ui/themes";
+import Image from "next/image";
+
+interface MovieCardProps {
+    name: string;
+    description: string;
+    img?: string;
+}
+
+const MovieCard = ({ name, description, img }: MovieCardProps) => {
+    return (
+        <Box className="mx-2 h-full">
+            <Card size="2" className="w-full h-full">
+                <Inset clip="padding-box" side="top" pb="current">
+                    { img? <Image
+                        src={img}
+                        alt={name}
+                        style={{
+                            display: "block",
+                            objectFit: "cover",
+                            width: "100%",
+                            height: "12rem",
+                            backgroundColor: "var(--gray-5)",
+                        }}
+                    />:
+                    <div className="h-48" style={{backgroundColor: "var(--gray-5)"}}></div>}
+                </Inset>
+                <div className="flex flex-col gap-2 p-4">
+                    <div className="font-semibold">
+                        {name}
+                    </div>
+                    <div className="text-sm line-clamp-4">
+                        {description}
+                    </div>
+                </div>
+            </Card>
+        </Box>
+    )
+}
+
+export default MovieCard;
