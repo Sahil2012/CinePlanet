fcfc8a86c2fc81b025019924762919efb628095f
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2024-12-15 15:14:55 +0530

    fix: lint issues in booking page

--- frontend/cineplanet/app/booking/page.tsx
+++ frontend/cineplanet/app/booking/page.tsx
@@ -1,36 +1,148 @@
 "use client";
-import SeatArrangement from "@/SeatArrangement";
+import SeatArrangement from "./SeatArrangement";
 import { useState } from "react";
 
 const seatLayout = [
-	["AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE"],
-	["AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE"],
-	["AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE"],
-	["AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE"],
-	["AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE"],
-	["AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE"],
-	["AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE","AVAILABLE"],
-	["AVAILABLE","AVAILABLE","AVAILABLE","NA","AVAILABLE","AVAILABLE","NA","AVAILABLE","AVAILABLE","AVAILABLE"],
-	["AVAILABLE","AVAILABLE","AVAILABLE","NA","AVAILABLE","AVAILABLE","NA","AVAILABLE","AVAILABLE","AVAILABLE"],
-	["AVAILABLE","AVAILABLE","AVAILABLE","NA","AVAILABLE","AVAILABLE","NA","AVAILABLE","AVAILABLE","AVAILABLE"]
-]
-
+  [
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+  ],
+  [
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+  ],
+  [
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+  ],
+  [
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+  ],
+  [
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+  ],
+  [
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+  ],
+  [
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+  ],
+  [
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "NA",
+    "AVAILABLE",
+    "AVAILABLE",
+    "NA",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+  ],
+  [
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "NA",
+    "AVAILABLE",
+    "AVAILABLE",
+    "NA",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+  ],
+  [
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+    "NA",
+    "AVAILABLE",
+    "AVAILABLE",
+    "NA",
+    "AVAILABLE",
+    "AVAILABLE",
+    "AVAILABLE",
+  ],
+];
 
 const Booking = () => {
-	const [seats, setSeats] = useState<string[][]>(seatLayout);
-	
-	const setSeat = (row: number, column: number, state: string) => {
-		seatLayout[row][column] = state;
-		setSeats(seatLayout);
-	}
+  const [seats, setSeats] = useState<string[][]>(seatLayout);
 
-	return (
-		<>
-			<div className="flex justify-center p-4">
-				<SeatArrangement seats={seats} rows={seatLayout.length} columns={seatLayout[0].length} setSeat={setSeat} />
-			</div>
-		</>
-	)
-}
+  const setSeat = (row: number, column: number, state: string) => {
+    seatLayout[row][column] = state;
+    setSeats(seatLayout);
+  };
+
+  return (
+    <div className="flex justify-center p-4">
+      <SeatArrangement
+        seats={seats}
+        rows={seatLayout.length}
+        columns={seatLayout[0].length}
+        setSeat={setSeat}
+      />
+    </div>
+  );
+};
 
 export default Booking;
