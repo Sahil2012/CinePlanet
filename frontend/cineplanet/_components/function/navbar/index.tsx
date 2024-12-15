6001daba698866c5d88bcfbc98b0d69808bafeb7
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2024-12-15 15:06:04 +0530

    update: add link to home page on logo

--- frontend/cineplanet/_components/function/navbar/index.tsx
+++ frontend/cineplanet/_components/function/navbar/index.tsx
@@ -3,17 +3,22 @@ import LogoText from "@/_assets/cine-planet.png";
 import { Flex } from "@radix-ui/themes";
 import Image from "next/image";
 import Profile from "./Profile";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <header className="h-20 py-5 flex justify-between items-center">
+    <header className="h-full py-5 flex justify-between items-center">
       <Flex gap="4" align="center" className="h-full">
-        <Image src={Logo} className="h-full w-auto" alt="CinePlanet Logo" />
-        <Image
-          src={LogoText}
-          className="h-full w-auto py-2"
-          alt="CinePlanet Logo Text"
-        />
+        <Link href="/" className="h-full">
+          <Image src={Logo} className="h-full w-auto" alt="CinePlanet Logo" />
+        </Link>
+        <Link href="/" className="h-full">
+          <Image
+            src={LogoText}
+            className="h-full w-auto py-2"
+            alt="CinePlanet Logo Text"
+          />
+        </Link>
       </Flex>
       <Flex>
         <Profile />
