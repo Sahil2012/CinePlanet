37f0ee48a6f6a5f42a84acc0c5ed4f4a97ea29e8
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2025-02-17 16:51:56 +0530

    fix: eslint issues in build

--- frontend/cineplanet/app/(home)/filter-control/AppliedFilters.tsx
+++ frontend/cineplanet/app/(home)/filter-control/AppliedFilters.tsx
@@ -1,9 +1,7 @@
 import BadgeGroup from "@/_components/ui/BadgeGroup";
 import { Button, Flex, Strong, Text } from "@radix-ui/themes";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import React from "react";
-import { IoCloseCircle } from "react-icons/io5";
-import { MdClose } from "react-icons/md";
 
 interface AppliedFiltersProps {
   selectedGenres: string[];
